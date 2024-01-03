4832d2c11889882c2b88f4159ce5ebed9ad94472
Charles Benassayag <charles.benassaya@natan.fr>
2024-01-03 12:45:15 +0200

    ADD

---
+++ Components/Tiveriad.Apis/Extensions/ExceptionHandlerExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Tiveriad.Apis.Contracts;
+using Tiveriad.Apis.Extensions;
+
+namespace Tiveriad.Apis;
+
+
+class ExceptionHandler { }
+
+
+public static class ExceptionHandlerExtensions
+{
+    /// <summary>
+    /// registers the default global exception handler which will log the exceptions on the server and return a user-friendly json response to the client
+    /// when unhandled exceptions occur.
+    /// TIP: when using this exception handler, you may want to turn off the asp.net core exception middleware logging to avoid duplication like so:
+    /// <code>
+    /// "Logging": { "LogLevel": { "Default": "Warning", "Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware": "None" } }
+    /// </code>
+    /// </summary>
+    /// <param name="logger">an optional logger instance</param>
+    /// <param name="logStructuredException">set to true if you'd like to log the error in a structured manner</param>
+    /// <param name="useGenericReason">set to true if you don't want to expose the actual exception reason in the json response sent to the client</param>
+    public static IApplicationBuilder UseDefaultExceptionHandler(this IApplicationBuilder app,
+        ILogger? logger = null,
+        bool logStructuredException = false,
+        bool useGenericReason = false)
+    {
+        app.UseExceptionHandler(
+            errApp =>
+            {
+                errApp.Run(
+                    async ctx =>
+                    {
+                        var exHandlerFeature = ctx.Features.Get<IExceptionHandlerFeature>();
+
+                        if (exHandlerFeature is not null)
+                        {
+                            logger ??= ctx.Resolve<ILogger<ExceptionHandler>>();
+                            var route = exHandlerFeature.Endpoint?.DisplayName?.Split(" => ")[0];
+                            var exceptionType = exHandlerFeature.Error.GetType().Name;
+                            var reason = exHandlerFeature.Error.Message;
+
+                            if (logStructuredException)
+                                logger.LogError("{@route}{@exceptionType}{@reason}{@exception}", route, exceptionType,
+                                    reason, exHandlerFeature.Error);
+                            else
+                            {
+                                logger.LogError(
+                                    $"""
+                                     =================================
+                                     { route} 
+                                     TYPE: { exceptionType} 
+                                     REASON: { reason} 
+                                     ---------------------------------
+                                     { exHandlerFeature.Error.StackTrace} 
+                                     """ );
+                            }
+
+                            ctx.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            ctx.Response.ContentType = "application/problem+json";
+                            await ctx.Response.WriteAsJsonAsync(
+                                new InternalErrorResponse
+                                {
+                                    Status = "Internal Server Error!",
+                                    Code = ctx.Response.StatusCode,
+                                    Reason = useGenericReason ? "An unexpected error has occurred." : reason,
+                                    Note = "See application log for stack trace."
+                                });
+                        }
+                    });
+            });
+
+        return app;
+    }
+}
---
+++ Tests/Tiveriad.Keycloak.Tests/Startup.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Tiveriad.Commons.Extensions;
+using Tiveriad.Keycloak.Apis;
+using Tiveriad.Keycloak.Models;
+using Tiveriad.Keycloak.Services;
+using Tiveriad.UnitTests;
+using Xunit;
+
+namespace Tiveriad.Keycloak.Tests;
+
+public class Startup : StartupBase
+{
+    public override void Configure(IServiceCollection services)
+    {
+        var factory = KeycloakSessionFactory.Configurator.Get(x =>
+        {
+            x.SetCredential("admin_user", "HlAe!26!BtLt").SetUrlBase("https://secure.kodin.info");
+        }).Build();
+
+        services.AddSingleton<IKeycloakSessionFactory>(factory);
+        
+        
+        services.AddScoped<HttpClient>( x =>
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://secure.kodin.info/auth/admin/realms/");
+            return httpClient;
+        });
+        
+        services.AddScoped<IRolesApi, RolesApi>();
+        services.AddScoped<IUsersApi, UsersApi>();
+        services.AddScoped<IRoleMapperApi, RoleMapperApi>();
+        
+    }
+}
+
+
+public class UserTestModule : TestBase<Startup>
+{
+    [Fact]
+    public async Task Add_User()
+    {
+        IUsersApi? usersApi = GetService<IUsersApi>();
+        var result = await usersApi.PostUser("kodin-dev-realm", new UserRepresentation()
+        {
+            Email = "charles.benassaya@gmail.com",
+            FirstName = "Charles",
+            LastName = "Benassaya",
+            Username = "charles.benassaya@gmail.com",
+            Credentials = new List<CredentialRepresentation>()
+            {
+                new CredentialRepresentation()
+                {
+                    Type = "password",
+                    Value = "J5x5omsg",
+                    Temporary = false
+                }
+            },
+            Attributes = new Dictionary<string, object>()
+            {
+                {"tenantId", "natan"},
+                {"tenantIds", new List<string>() {"one", "two"}},
+                {"locale", "fr"}
+            },
+            Enabled = true,
+            EmailVerified = true
+        });
+        Assert.True(result.Data);
+    }
+    
+    [Fact]
+    public async Task Get_User()
+    {
+        IUsersApi? usersApi = GetService<IUsersApi>();
+        var response = await usersApi.GetUsersByRealm("kodin-dev-realm", false, "charles.benassaya@gmail.com", first: 0, max:10);
+        var result = response.Data.FirstOrDefault();
+        
+        Assert.True(response.Data!=null);
+    }
+    
+    
+    [Fact]
+    public async Task Put_User()
+    {
+        IUsersApi? usersApi = GetService<IUsersApi>();
+        var response = await usersApi.GetUsersByRealm("kodin-dev-realm", false, "charles.benassaya@gmail.com", first: 0, max:10);
+        var result = response.Data.FirstOrDefault();
+        
+        Assert.True(response.Data!=null);
+    }
+}
